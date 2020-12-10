54d10bf6eb1d62066e91e9d8f9b9e21ecfd1d6ad
Shubham Samal <samalshubham67@gmail.com>
2020-12-10 14:43:33 +0530

    URCALC

---
+++ cal.py
@@ -0,0 +1,25 @@
+a = int(input(""))
+b = int(input(""))
+
+# choose operator
+
+c = input("")
+
+if c == "+":
+    d = a + b
+    print(d)
+
+elif c == "-":
+    e = a - b
+    print(e)
+
+elif c == "/":
+    f = a/b
+    print(f)
+
+elif c == "*":
+    g = a*b
+    print(g)
+
+else :
+    print("Invalid Operator")
